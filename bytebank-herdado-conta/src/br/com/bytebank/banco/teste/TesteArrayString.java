7d4bbaca519c00c8b9c00a88020368cef35200d1
bianavic <bianavic@gmail.com>
2020-01-14 13:36:19 -0300

    array String args

---
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/teste/TesteArrayString.java
@@ -0,0 +1,25 @@
+// String[] = interagir com sua aplicacao java a partir da linha de comando
+
+// utilidades: configuracoes de logs
+
+// existe para que possamos passar parâmetros a partir do terminal,
+// por exemplo, passaremos <<1 2 oi nico java>>, ou seja, 5 parâmetros
+
+//  uma forma de interagir com o programa Java a partir da linha de comando.
+//  Chamamos a aplicação e, simultaneamente, é possível definirmos parâmetros
+package br.com.bytebank.banco.teste;
+
+import br.com.bytebank.banco.modelo.Cliente;
+
+public class TesteArrayString {
+
+    public static void main(String[] args) {
+
+        System.out.println("Funcionou!!");
+
+        // array string args (alterar configuracoes no intellj
+//        for (int i = 0; i < args.length; i++);
+//        System.out.println(args[0]);
+
+    }
+}
